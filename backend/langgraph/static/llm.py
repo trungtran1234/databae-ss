b852b4e052c8c9b1f3b9d91d7b86d7759ef6cc37
Trung Tran <trungtran63470@gmail.com>
2024-10-24 23:56:45 -0700

    add load_env()

--- backend/langgraph/static/llm.py
+++ backend/langgraph/static/llm.py
@@ -1,6 +1,8 @@
 from langchain_groq import ChatGroq
 import os
+from dotenv import load_dotenv
 
+load_dotenv()
 llm=ChatGroq(temperature=0,
              model_name="llama3-70b-8192",
              api_key=os.getenv("GROQ_API_KEY"))
