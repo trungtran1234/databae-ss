38f977b9c3755ec871ab6714bcfd313f89d889af
Trung Tran <48637790+trungtran1234@users.noreply.github.com>
2024-10-25 12:34:12 -0700

    added generator node boilerplate (#10)
    * generator agent boilerplate
    
    * boilerplate
    
    * fix import
    
    * add load_env()
    
    * refactor langgraph to agent_network
    
    * fix deprecated method, made it work woohoo
    
    * generator node boilerplate

---
+++ backend/agent_network/agents/agent_helper.py
@@ -0,0 +1,30 @@
+from pydantic import BaseModel
+from typing_extensions import TypedDict
+from typing import Annotated, Sequence
+from langchain_core.messages import (
+    BaseMessage,
+    HumanMessage,
+    ToolMessage,
+    AIMessage
+)
+import operator
+
+# class QueryRequest(BaseModel):
+#     user_query: str
+#     system_message: str
+
+class AgentState(TypedDict):
+    messages: Annotated[Sequence[BaseMessage], operator.add]
+    sender: str
+
+def agent_node(state, agent, name):
+    result = agent.invoke(state)
+    # We convert the agent output into a format that is suitable to append to the global state
+    if isinstance(result):
+        pass
+    else:
+        result = AIMessage(**result.dict(exclude={"type", "name"}), name=name)
+    return {
+        "messages": [result],
+        "sender": name,
+    }
