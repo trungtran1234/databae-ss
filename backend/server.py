6c08c63d1e405e58841b108402bc403386218ad8
Trung Tran <48637790+trungtran1234@users.noreply.github.com>
2024-10-24 23:51:23 -0700

    Generator agent boilerplate (#5)
    * generator agent boilerplate
    
    * boilerplate

--- backend/server.py
+++ backend/server.py
@@ -1,4 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from langgraph.agents.agent_class import QueryRequest
+from langgraph.agents.generator import query_generator
+from langgraph.static.llm import llm
 
 app = FastAPI()
 
@@ -6,3 +9,14 @@ app = FastAPI()
 def root():
     return "hello world"
 
+@app.post("/submit")
+def generate_sql(query_request: QueryRequest):
+    try:
+        sql_query = query_generator(
+            llm=llm, 
+            system_message=query_request.system_message, 
+            user_message=query_request.user_query
+        )
+        return {"sql_query": sql_query}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
